74426fbf438924149e6715d5d5eae3041affe032
hakan yarimaga <h.yarimaga@HakanMacBookPro.local>
2019-03-15 17:43:38 +0300

    first commit

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Elastic_Kibana_DotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace Elastic_Kibana_DotNetCore.Controllers {
+    public class HomeController : Controller {
+        private ILogger<HomeController> _logger;
+
+        public HomeController (ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+        public IActionResult Index () {
+            _logger.LogInformation ($"Index");
+            _logger.LogError("ooooppsss");
+                        _logger.LogCritical("ooooppsss critical");
+
+            return View ();
+        }
+
+        public IActionResult Privacy () {
+            _logger.LogInformation ($"Privacy");
+            return View ();
+        }
+
+        [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error () {
+            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Elastic_Kibana_DotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
